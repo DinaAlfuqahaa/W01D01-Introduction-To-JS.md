84675badfa61ff9bb3437718e6293eab9e7cfec8
DinaAlfuqahaa <154809410+DinaAlfuqahaa@users.noreply.github.com>
2024-01-04 02:05:33 -0800

    practice 1 to 12

---
+++ main.js
@@ -0,0 +1,73 @@
+/* START CODE UNDER THIS LINE */
+//q1
+9 + 1 * 5; // 14
+9 % 2; // 1
+(9 / 3) * (10 - 7); // 9
+21 / 3 + 3 * 9 * 9 + 5; // 255
+36 / 9 + 48 - 10 / 2; // 47
+//Q2
+const @name = 'John';// const $name='John'
+const 'age' = 10 ;// const age = 10 
+const const = "Constant"; //const constant ="constant"
+const variable 1 = "value";// const variable1="value"
+//q3
+//Define the following variables.
+
+//Define a variable color containing your favorite color.//const color ='blue'
+
+//Define a variable positiveNumber containing any positive number.//const positiveNumber =7
+
+//Define a variable food containing your favorite food.//const food ='pizza'
+
+//Define a variable PhoneNumber containing a phone number.// const PhoneNumber='526354'
+//  q  4
+10 + "10"; // 1010
+"10" + "10"; // 1010
+10 + 10 + "5"; // 205
+"Hello" + 1; // 'Hello1'
+"10" + 5 + 5; // 1055
+"20" - 10; // 10
+"9" - "5"; // 4
+"19" - "13" + 17; // 23
+5 + 6 + "4" + 9 - 4 - 2; // 1143
+//q5 Using string concatenation write an expression that represents your first name, last name, gender, and nationality.
+let firstName ='Dina' 
+let lastName ='Fugahaa'
+let gender = "Female"
+let nationality ="jordanian"
+let personalinf=firstName +lastName+gender+nationality
+//q6 Write an expression that represents the number of seconds in 30 days.
+let s30d=30*24*60*60 ;//2592000
+//q7 eassign the value of a to make the following expression equal to nine.
+let a;
+let b = 3;
+let c = 10;
+
+c - b * c + a;//a=29
+//q8 Using the variables below write an expression that calculates the total amount for buying two coffee cups.
+//// tip and tax amounts are a percentage of the total billed amount
+const tip = 0.1;
+const taxRate = 0.08;
+const coffeeCupPrice = 6;//let totalAmount=2*coffeCupPrice*(1+tip+taxRate)
+
+//q9 Write an expression that represents the average grade of an exam, knowing that in a class of ten students, five of them got 24/30, two of them got 16/30 and the remainder got 29/30.
+let average=(5*24+2*16+3*29)/10 ;//23.9
+//q10 Write expressions that calculate the area and the perimeter of a rectangle with a length of 10m and a width of 5m.
+
+let length =10 
+let width=5
+let area =length *width ;//50
+//q11Write expressions that calculate the diameter and the circumference of a circle with a radius of 5m.
+let radius =5;
+let diameter =2*radius ;//10
+let circumference=2*Math.pi*radius;//62.8
+
+
+//q12 Write an expression that converts 30 degrees Celsius to Fahrenheit.
+let Celsius=30;
+let Fahrenheit = (Celsius*9/5+32);//86 Fehrenheit
+
+
+
+
+
